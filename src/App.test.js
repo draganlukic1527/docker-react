8178d7d2f82b014e32454755b1facc2cbf0eb737
draganlukic1527 <draganlukic1527@gmail.com>
2021-09-21 22:12:14 -0700

    changed test environment

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Docker React Test Run Dragan/i);
+  const linkElement = screen.getByText(/Docker React Test Run Dragan/);
   expect(linkElement).toBeInTheDocument();
 });
