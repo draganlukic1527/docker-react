e02c980bfecfeeff14a7c966ec415527b09e253f
draganlukic1527 <draganlukic1527@gmail.com>
2021-09-21 22:06:36 -0700

    changed text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        I was changed on the feature branch
+        Docker Test changed Text
       </header>
     </div>
   );
